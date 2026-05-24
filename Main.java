f573c59d1f019183273fe3285048806a1590c55c
CodeHunt7 <mishavasilenko1@yandex.ru>
2026-05-25 01:19:46 +0300

    Update project

--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayStatsTask.printStats();
+        //ArrayStatsTask.printStats();
 
+        int result = 2 + 3 * 4;
+        System.out.println(result);
     }
 }
